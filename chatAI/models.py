11cb2ceabb8d55abd73fcaf25dd3aeb693c57f56
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-03-21 22:13:37 +0300

    create ChatHistory

--- chatAI/models.py
+++ chatAI/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ChatHistory(models.Model):
+    text = models.CharField(max_length=5000)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # Связь с пользователем
+    created_at = models.DateTimeField(auto_now_add=True)
+    sender_type = models.CharField(max_length=5)
