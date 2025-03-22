926824409cef1890b370bb68778f163c20ad4ddf
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-03-22 09:48:58 +0300

    add all base tables recipe

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+from baseAPI import settings
+
+
+class Recipe(models.Model):
+    autor_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=2000)
+    instructions = models.TextField(max_length=100000)
+    cooking_time_minutes = models.IntegerField()
+    servings = models.IntegerField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_private = models.BooleanField(default=False)
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class RecipeIngredients(models.Model):
+    id_ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    id_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    count_in_grams = models.IntegerField()
+    visible_type_of_count = models.CharField(max_length=25)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['id_ingredient', 'id_recipe'], name='unique_ingredient_recipe'
+            )
+        ]
+
+
+class Likes(models.Model):
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['recipe_id', 'user_id'], name='unique_likes_recipe'
+            )
+        ]
+
+
+class SearchHistory(models.Model):
+    text = models.TextField(max_length=100)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment_text = models.TextField(max_length=2000)
