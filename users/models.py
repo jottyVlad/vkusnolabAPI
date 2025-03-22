2c31b8a3f937be9dd161cfc5296430dcfa38e4d7
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-03-22 10:08:33 +0300

    fix followers

--- users/models.py
+++ users/models.py
@@ -11,8 +11,8 @@ class CustomUser(AbstractUser):
 
 
 class Followers(models.Model):
-    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
