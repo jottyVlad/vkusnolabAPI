edd9b236eb847d889e519d6469cb65b01f655fef
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-03-22 09:20:47 +0300

    create Followers

--- users/models.py
+++ users/models.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from baseAPI import settings
+
+
 class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
+    #TODO: разобраться что за путь к картинке
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     registered_on = models.DateTimeField(auto_now_add=True)
 
+
+class Followers(models.Model):
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user_id', 'author_id'], name='unique_followers'
+            )
+        ]
