a71e694d7bf104a6f9097cb115094e65e4fb11c0
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-03-25 11:29:35 +0300

    create cart & views

--- mongo/models.py
+++ mongo/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from mongoengine import *
 
-# Create your models here.
+class Cart(Document):
+    user_id = fields.StringField(required=True)
+    count_in_grams = fields.IntField()
+    ingredient_id = fields.StringField(required=True)
+
+class Views(Document):
+    user_id = fields.StringField(required=True)
+    recipe_id = fields.StringField(required=True)
