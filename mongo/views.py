fed57145bfa673a102a6eaee4fc01d1d838d5932
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-03-25 11:33:05 +0300

    add method to get db

--- mongo/views.py
+++ mongo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from pymongo import MongoClient
 
-# Create your views here.
+def get_db_handle():
+    """get mongoDB"""
+    client = MongoClient(host="mongodb://localhost", port=27017)
+    db_handle = client.djangoTutorial.to_do_item
+    return db_handle
