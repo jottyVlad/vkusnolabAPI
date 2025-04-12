342b86190e1368d66b3a0d411fbaaa2878cbe379
jotty <bard143games@gmail.com>
2025-04-12 09:52:46 +0300

    added user endpoints and swagger files

--- users/serializers.py
+++ users/serializers.py
@@ -23,3 +23,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
