d7abec3c618105f4a03320cb3aa55c75d80b1cff
Evgenia Morozova <72866082+Morevq@users.noreply.github.com>
2025-05-02 20:30:50 +0300

    for all methods

--- recipe/permissions.py
+++ recipe/permissions.py
@@ -2,6 +2,6 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in ['PUT', 'PATCH', 'DELETE']:
-            return obj.author_id == request.user
-        return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
